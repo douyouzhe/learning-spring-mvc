d167553684ddc9965433b27aac12be15ef45bc6c
douyouzhe <douyouzhe@gmail.com>
2017-11-25 02:48:58 -0500

    1

--- src/com/dou/controller/HelloController.java
+++ src/com/dou/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.dou.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
@@ -28,10 +27,19 @@ import org.springframework.web.servlet.mvc.AbstractController;
  With annotation
  */
 @Controller
+@RequestMapping("/greet")//class level annotation
 public class HelloController{
     // front controller sees the request mapping and decide which method to execute
-    @RequestMapping("/welcome")
+    @RequestMapping("/hello") // response to /greet/hello
     public ModelAndView helloWorld(){
+        //to prepare the view mapping
+        ModelAndView modelAndView = new ModelAndView("HelloPage");
+        modelAndView.addObject("welcomeMessage","Hello!!!! There");
+        return modelAndView;
+    }
+
+    @RequestMapping("/hi")// response to /greet/hi
+    public ModelAndView hiWorld(){
         //to prepare the view mapping
         ModelAndView modelAndView = new ModelAndView("HelloPage");
         modelAndView.addObject("welcomeMessage","Hi!!!!!!!!!!! There");
